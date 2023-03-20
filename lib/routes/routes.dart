85ae77cc1d68fc01dfd415958c79c39af066499f
cavin6080 <cavinmacwan1@gmail.com>
2023-03-21 00:21:32 +0530

    code fixes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_route_demo/routes/routes.gr.dart';
 
-@AutoRouterConfig(
-  replaceInRouteName: 'Page,Route',
-)
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends $AppRouter {
   @override
   RouteType get defaultRouteType => const RouteType.material();
