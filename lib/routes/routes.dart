51230cb500f012edf6c627bc66f9a83539ebd8b9
cavin6080 <cavinmacwan1@gmail.com>
2023-03-21 00:26:02 +0530

    [ADD] Comments

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,26 +1,23 @@
-import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:auto_route_demo/pages/profile_page.dart';
-import 'package:auto_route_demo/pages/home_page.dart';
-import 'package:auto_route_demo/pages/tab_page.dart';
 import 'package:auto_route_demo/routes/routes.gr.dart';
 
-@AutoRouterConfig(
-  replaceInRouteName: 'Page,Route',
-)
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends $AppRouter {
   @override
-  RouteType get defaultRouteType => RouteType.material();
+  RouteType get defaultRouteType => const RouteType.material();
   @override
   final List<AutoRoute> routes = [
     AutoRoute(
       path: "/",
       page: TabRoute.page,
       children: [
+        // First bottomnavigation bar tab
         AutoRoute(
           path: 'home',
           page: HomeRouter.page,
         ),
+
+        // Second bottomnavigation bar tab
         AutoRoute(
           path: "profile",
           page: ProfileRouter.page,
